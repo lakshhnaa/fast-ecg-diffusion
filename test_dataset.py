eefea24fd5c802f8c85f7e87c108ee599ef13ee4
Lakshnaa <lakshnaabj@gmail.com>
2026-02-26 18:38:40 +0530

    Initialize DataLoader for ECGDataset
    Add DataLoader for ECGDataset and print shapes

--- test_dataset.py
+++ test_dataset.py
@@ -1,1 +1,9 @@
+from data.dataset import ECGDataset
+from torch.utils.data import DataLoader
 
+dataset = ECGDataset(record_ids=['100'], window_size=512)
+loader = DataLoader(dataset, batch_size=4, shuffle=True)
+
+for noisy, clean in loader:
+    print(noisy.shape, clean.shape)
+    break
